8cac8058067d36572bec8a3f325775aae0cf782d
zhuxp <nimezhu@gmail.com>
2017-01-26 15:24:17 -0500

    improve width and height

--- scope/lib.js
+++ scope/lib.js
@@ -1,21 +1,31 @@
 (function(d3,$){
-  var main;
-  main = d3.select("#main")
-  var scope = {};
+  var body = d3.select("body")
+  var scope = {}
+  var top = body.append("div").attr("id","top")
+    .classed("container",true)
+  var main = body.append("div").attr("id","main")
+    .classed("container",true)
+  var left = body.append("div").attr("id","left")
+    .classed("container",true)
+  top.append("label").attr("for","width").text("width")
+  var width = top.append("span").attr("id","width")
+  top.append("label").attr("for","height").text("height")
+  var height = top.append("span").attr("id","height")
+  var canvas = main.append("canvas")
   $(window).resize(function() {
       resize()
-  });
+  })
   var resize = function() {
       //var canvasdiv = d3.select("#canvasdiv")
-      scope.canvas = d3.select("#main").append("canvas")
-      main.style("height", $(window).height() + "px")
-      main.style("width", ($(window).width() - 200) + "px")
       scope.width = $(window).width() - 200
-      scope.height = $(window).height()
-      d3.select("#width").text(scope.width)
-      d3.select("#height").text(scope.height)
+      scope.height = $(window).height() - 30
+      //canvas.attr("width", scope.width).attr("height", scope.height)
+      main.style("height", scope.height+ "px")
+      main.style("width", scope.width + "px")
+      width.text(scope.width)
+      height.text(scope.height)
       console.log(scope)
-      scope.canvas.attr("width", scope.width).attr("height", scope.height)
+
   }
   resize()
 }(d3,jQuery))
