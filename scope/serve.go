cb2af8357f9da0eaa3035d3c8ff515adf414cd51
zhuxp <nimezhu@gmail.com>
2017-02-08 15:30:36 -0500

    add tracks option

--- scope/serve.go
+++ scope/serve.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"encoding/csv"
+	"io"
 	"log"
 	"net/http"
+	"path"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/nimezhu/snowjs"
@@ -13,28 +17,54 @@ import (
 	"github.com/urfave/cli"
 )
 
-/* CmdServe: serve bigwig data
- *     	     prototype serve one bigwig file now.
+func readBw(uri string) *BigWigReader {
+	reader, err := stream.NewSeekableStreamReader(uri)
+	checkErr(err)
+	bwf := NewBbiReader(reader)
+	bwf.InitIndex()
+	log.Println("in reading idx of", uri)
+	bw := NewBigWigReader(bwf)
+	return bw
+}
+
+func serveBwURI(uri string, router *mux.Router, prefix string) {
+	bwExt := strings.ToLower(path.Ext(uri))
+	bwmap := make(map[string]*BigWigReader)
+	if bwExt == ".bw" || bwExt == ".bigwig" { //Single BigWig File to Default
+		bwmap["default"] = readBw(uri)
+	} else if bwExt == ".txt" || bwExt == ".tsv" {
+		reader, err := stream.NewSeekableStreamReader(uri)
+		checkErr(err)
+		r := csv.NewReader(reader)
+		r.Comma = '\t'
+		for {
+			record, err := r.Read()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatal(err)
+			}
+			log.Println(record)
+			bwmap[record[0]] = readBw(record[1])
+		}
+	}
+	AddBwsHandle(router, bwmap, prefix)
+}
+
+/* CmdServe: serve bigwigs and hic, and static html
+ *
  */
 func CmdServe(c *cli.Context) error {
-	bwURI := c.String("B")
-	log.Println(bwURI)
-	hicURI := c.String("H")
 	port := c.Int("port")
 	router := mux.NewRouter()
 	snowjs.AddHandlers(router, "")
 	AddStaticHandle(router)
-	reader, err := stream.NewSeekableStreamReader(bwURI)
-	checkErr(err)
-	/* TODO Multi Tracks */
-	bwf := NewBbiReader(reader)
-	bwf.InitIndex()
-	log.Println("in reading idx")
-	bw := NewBigWigReader(bwf)
-	bwmap := make(map[string]*BigWigReader)
-	bwmap["default"] = bw
-	/* End of Test */
-	AddBwsHandle(router, bwmap, "/bw")
+
+	bwURI := c.String("B")
+	serveBwURI(bwURI, router, "/bw")
+	/* TODO: multi hic files */
+	hicURI := c.String("H")
 	hicreader, err := stream.NewSeekableStreamReader(hicURI)
 	hic, err := HiC.DataReader(hicreader)
 	checkErr(err)
