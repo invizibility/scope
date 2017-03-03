26aae0435a5c710091bb68a480d28c90e37d2427
zhuxp <nimezhu@gmail.com>
2017-03-03 13:24:47 -0500

    clean redunce

--- scope/web/lib/brushtri.js
+++ scope/web/lib/brushtri.js
@@ -18,21 +18,25 @@ var snow = snow || {};
             y = 0; //x,y is the coord system start point?
         var theta = 0
         var scale = d3.scaleLinear().range([0, 500]).domain([0, 500])
-        var yscale = d3.scaleLinear().range([0,500]).domain([500,0]) //domain reverse.
+        var yscale = d3.scaleLinear().range([0, 500]).domain([500, 0]) //domain reverse.
+
         var G
-        var flag = function(selection) {
-          selection.each(function(d){
-            d3.select(this)
-              .attr("transform",function(d){return "translate("+d.x+","+d.y+")"})
-            var path = d3.select(this).selectAll(".flag")
-            .data([d])
-            path.enter().append("path").classed("flag",true)
-            .merge(path)
-            .attr("d", d3.symbol().type(S.symbolFlag).size(d.size))
-            .style("fill","black")
-            .style("opacity",0.2)
-          })
+        var flag = function (selection) {
+            selection.each(function (d) {
+                d3.select(this)
+                    .attr("transform", function (d) {
+                        return "translate(" + d.x + "," + d.y + ")"
+                    })
+                var path = d3.select(this).selectAll(".flag")
+                    .data([d])
+                path.enter().append("path").classed("flag", true)
+                    .merge(path)
+                    .attr("d", d3.symbol().type(S.symbolFlag).size(d.size))
+                    .style("fill", "black")
+                    .style("opacity", 0.2)
+            })
         }
+        var listeners = d3.dispatch(brush, "start", "brush", "end", "click", "activate", "deactivate","brush.local")
         var brush = function (selection) {
             G = selection.append("g").attr("transform", "translate(" + x + "," + y + ") rotate(" + theta / Math.PI * 180 + ")")
             var tri = G.append("path")
@@ -53,11 +57,11 @@ var snow = snow || {};
             rect.on("click", function (e) {
                 listeners.call("click")
             })
-            listeners.on("deactivate", function(d){
-              rect.attr("opacity",0.0)
+            listeners.on("deactivate", function (d) {
+                rect.attr("opacity", 0.0)
             })
-            listeners.on("activate", function(d){
-              rect.attr("opacity",0.2)
+            listeners.on("activate", function (d) {
+                rect.attr("opacity", 0.2)
             })
             var fix = function (x, y) {
                 if (x + y + width + height > edge) {
@@ -133,8 +137,8 @@ var snow = snow || {};
                 g.attr("transform", "translate(" + Math.min(x0, x1) + "," + Math.min(y0, y1) + ")")
                 rect.attr("height", height).attr("width", width)
                 listeners.call("brush", this, invert([
-                    [Math.min(x0,x1), Math.min(y0,y1)],
-                    [Math.min(x0,x1) + width, Math.min(y0,y1) + height]
+                    [Math.min(x0, x1), Math.min(y0, y1)],
+                    [Math.min(x0, x1) + width, Math.min(y0, y1) + height]
                 ]));
             }
 
@@ -143,39 +147,41 @@ var snow = snow || {};
                 //listeners.call("end", this, [[p[0],yi],[xi+width,yi+height]]);
             }
         }
-        var listeners = d3.dispatch(brush, "start", "brush", "end", "click","activate","deactivate","response")
-        listeners.on("response",function(d){
-          var data =
-            [{"x":scale(d[0]),"y":yscale(d[1]),"size":scale(d[1])-scale(d[0])},
-             //{"x":scale(d[0][1]),"y":yscale(d[1][1]),"size":scale(d[1][1])-scale(d[0][1])}
-           ]
-          var b = G.selectAll(".rLite").data(data)
-           b.enter().append("g").classed("rLite",true)
-           .merge(b)
-           .call(flag)
+        var highlight=function(d) {
+            var data
+            if (typeof d[0] == "number") {
+                data = [{
+                        "x": scale(d[0]),
+                        "y": yscale(d[1]),
+                        "size": scale(d[1]) - scale(d[0])
+                    },
+                    //{"x":scale(d[0][1]),"y":yscale(d[1][1]),"size":scale(d[1][1])-scale(d[0][1])}
+                ]
+            } else {
+                data = [{
+                        "x": scale(d[0][0]),
+                        "y": yscale(d[1][0]),
+                        "size": scale(d[1][0]) - scale(d[0][0])
+                    },
+                    {
+                        "x": scale(d[0][1]),
+                        "y": yscale(d[1][1]),
+                        "size": scale(d[1][1]) - scale(d[0][1])
+                    }
+                ]
+            }
+            var b = G.selectAll(".hLite").data(data)
+            //b.exit().remove()
+            b.enter().append("g").classed("hLite", true)
+            .merge(b)
+            .call(flag)
+        }
 
-
+        listeners.on("deactivate.local", function (d) {
+            G.selectAll(".hLite").remove()
         })
-        listeners.on("deactivate.tri",function(d){
-          G.selectAll(".rLite").remove()
-          G.selectAll(".hLite").remove()
-        })
-        listeners.on("brush.local",function(d){
-        var data =
-          [{"x":scale(d[0][0]),"y":yscale(d[1][0]),"size":scale(d[1][0])-scale(d[0][0])},
-           {"x":scale(d[0][1]),"y":yscale(d[1][1]),"size":scale(d[1][1])-scale(d[0][1])}
-         ]
-
-         var b = G.selectAll(".hLite").data(data)
-          b.enter().append("g").classed("hLite",true)
-          .merge(b)
-          .call(flag)
-
-          listeners.on("activate.tri", function(d){
-            G.selectAll(".rLite").remove()
-          })
-
-          //listeners.call("brush",this,d)
+        listeners.on("brush.local", function(d){
+          highlight(d)
         })
         brush.theta = function (_) {
             return arguments.length ? (theta = _, brush) : theta;
@@ -191,30 +197,28 @@ var snow = snow || {};
             return arguments.length ? (y = _, brush) : y;
         }
         brush.domain = function (_) {
-            return arguments.length ? (scale.domain(_), yscale.domain([_[1],_[0]]), brush) : scale.domain();
+            return arguments.length ? (scale.domain(_), yscale.domain([_[1], _[0]]), brush) : scale.domain();
         }
-        brush.activate = function(_) {
-          listeners.call("activate",this,_)
+        brush.activate = function (_) {
+            listeners.call("activate", this, _)
         }
-        brush.deactivate = function(d) {
-          listeners.call("deactivate",this,d)
-          /** TO FIX THIS **/
+        brush.deactivate = function (d) {
+            listeners.call("deactivate", this, d)
+            /** TO FIX THIS **/
 
         }
-        brush.response = function(d) {
-          //TODO.
-          console.log("response",d)
-          listeners.call("response",this,d)
+        brush.process = function (d) {
+            if (d.code == "brush") {
+                highlight(d.d)
+            }
         }
-
         brush.scale = function (_) {
             return arguments.length ? (scale = _, edge = scale.range()[1] - scale.range()[0], yscale.domain([scale.domain()[1], scale.domain()[0]]).range(scale.range()), brush) : scale;
         }
 
         brush.edge = function (_) {
-            return arguments.length ? (edge = _, scale.range([0, edge]),yscale.range([0,edge]), brush) : edge;
+            return arguments.length ? (edge = _, scale.range([0, edge]), yscale.range([0, edge]), brush) : edge;
         }
-        brush.listeners = function(_) { return arguments.length ?(listeners= _, brush) : listeners; }
         return brush
     }
 
