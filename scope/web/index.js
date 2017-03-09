e1543ee104fac92d9d588113934d6c14dddce5aa
zhuxp <nimezhu@gmail.com>
2017-03-09 14:03:19 -0500

    add simpleMonitor template

--- scope/web/index.js
+++ scope/web/index.js
@@ -21,3 +21,6 @@ export{default as dataHic} from "./src/data/hic2"
 
 export{default as toolsGetUrlParam} from "./src/tools/getUrlParam"
 export{default as toolsRandomString} from "./src/tools/randomString"
+
+
+export{default as simpleMonitor} from "./src/simpleMonitor"
