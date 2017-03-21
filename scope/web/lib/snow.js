2369f78596d50f45faef589b7d9a72812774ce5e
zhuxp <nimezhu@gmail.com>
2017-03-21 15:01:14 -0400

    add fixRegions

--- scope/web/lib/snow.js
+++ scope/web/lib/snow.js
@@ -2292,6 +2292,30 @@ var parseRegions = function(s) {
   return r
 };
 
+function nearby(a,b) {
+    if (a.chr!=b.chr) {return false}
+    var l = Math.max(a.end,b.end) - Math.min(a.start,b.start);
+    if (((a.end-a.start)+(b.end-b.start))/ l > 0.95) {
+      return true
+    }
+    return false
+}
+function merge(a,b) {
+    return {"chr":a.chr,"start":Math.min(a.start,b.start),"end":Math.max(a.end,b.end)}
+}
+/* input :regions
+ * output : merged regions */
+function fixed(regions) { //for now it use two only.//TODO multi regions.
+  if (nearby(regions[0],regions[1])) {
+    return [merge(regions[0],regions[1])]
+  } else {
+    return regions
+  }
+}
+var fixRegions = function(regions) {
+  return fixed(regions)
+};
+
 var addPanelTo = function (el) {
     var panel = el.append("div").classed("panel", true);
     var head = panel.append("div").classed("panel-heading", true);
@@ -2414,6 +2438,7 @@ exports.dataHic = hic2;
 exports.toolsGetUrlParam = getUrlParam;
 exports.toolsRandomString = randomString;
 exports.toolsParseRegions = parseRegions;
+exports.toolsFixRegions = fixRegions;
 exports.simpleMonitor = simpleMonitor;
 exports.panel = panel;
 
