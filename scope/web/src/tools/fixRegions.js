0a870867a1d66e5a4d356d34a856962d17a83050
zhuxp <nimezhu@gmail.com>
2017-03-22 13:54:13 -0400

    layout basic functional

---
+++ scope/web/src/tools/fixRegions.js
@@ -0,0 +1,23 @@
+function nearby(a,b) {
+    if (a.chr!=b.chr) {return false}
+    var l = Math.max(a.end,b.end) - Math.min(a.start,b.start)
+    if (((a.end-a.start)+(b.end-b.start))/ l > 0.95) {
+      return true
+    }
+    return false
+}
+function merge(a,b) {
+    return {"chr":a.chr,"start":Math.min(a.start,b.start),"end":Math.max(a.end,b.end)}
+}
+/* input :regions
+ * output : merged regions */
+function fixed(regions) { //for now it use two only.//TODO multi regions.
+  if (nearby(regions[0],regions[1])) {
+    return [merge(regions[0],regions[1])]
+  } else {
+    return regions
+  }
+}
+export default function(regions) {
+  return fixed(regions)
+}
