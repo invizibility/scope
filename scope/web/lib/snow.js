ed67b91828257245638297bbe192f21cca0cbb49
zhuxp <nimezhu@gmail.com>
2017-03-23 17:36:41 -0400

    history uniq and envvent on show for monitors

--- scope/web/lib/snow.js
+++ scope/web/lib/snow.js
@@ -2461,14 +2461,28 @@ var panel = function () {
 };
 
 var simple = function(layout, container, state) {
+
     var div1 = d3.select(container.getElement()[0]).append("div");
     var div2 = d3.select(container.getElement()[0]).append("div");
+    var brush =[];
+    var update = [];
     layout.eventHub.on("brush", function(d) {
-        div2.html("BRUSHING   " + regionsText(d));
+        brush = d;
+        if(!container.isHidden){
+          div2.html("BRUSHING   " + regionsText(d));
+        }
+
     });
     layout.eventHub.on("update", function(d) {
-        div1.html("CURRENT   " + regionsText(d));
-        div2.html("");
+        update = d;
+       if(!container.isHidden){
+         div1.html("CURRENT   " + regionsText(d));
+         div2.html("");
+       }
+    });
+    container.on("show",function(d) {
+      div1.html("WAKEUP "+ regionsText(update));
+      div2.html("WAKEUP BRUSHING "+ regionsText(brush));
     });
 };
 
--- scope/web/src/renderer/simple.js
+++ scope/web/src/renderer/simple.js
@@ -1,13 +1,27 @@
 import regionText from "../tools/regionText"
 import regionsText from "../tools/regionsText"
 export default function(layout, container, state) {
+
     var div1 = d3.select(container.getElement()[0]).append("div");
     var div2 = d3.select(container.getElement()[0]).append("div");
+    var brush =[]
+    var update = []
     layout.eventHub.on("brush", function(d) {
-        div2.html("BRUSHING   " + regionsText(d))
+        brush = d
+        if(!container.isHidden){
+          div2.html("BRUSHING   " + regionsText(d))
+        }
+
     })
     layout.eventHub.on("update", function(d) {
-        div1.html("CURRENT   " + regionsText(d))
-        div2.html("")
+        update = d;
+       if(!container.isHidden){
+         div1.html("CURRENT   " + regionsText(d))
+         div2.html("")
+       }
+    })
+    container.on("show",function(d) {
+      div1.html("WAKEUP "+ regionsText(update))
+      div2.html("WAKEUP BRUSHING "+ regionsText(brush))
     })
 }
