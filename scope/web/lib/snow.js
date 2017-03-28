dfe826b27a62bd1b9902b2aeab2192354cb64099
zhuxp <nimezhu@gmail.com>
2017-03-28 16:29:51 -0400

    scope e5 prototype

--- scope/web/lib/snow.js
+++ scope/web/lib/snow.js
@@ -2467,12 +2467,43 @@ var panel = function () {
     return chart
 };
 
+var defaultConfig = {
+  "color" : "#EED"
+};
 var simple = function(layout, container, state) {
+    //local event driven . cfg, content ...
+    // not d3.call but registerComponent render.
+    // similar with d3.call.
+    // layout is the interface
+    // container is the element.
+    //var dispatch = d3.dispatch("local")
+    var cfg = d3.select(container.getElement()[0]).append("div").classed("cfg",true);
+    var content = d3.select(container.getElement()[0]).append("div").classed("content",true);
+    var div1 = content.append("div");
+    var div2 = content.append("div");
+    //state.config parameters.
+    /* render config panel and configs */
 
-    var div1 = d3.select(container.getElement()[0]).append("div");
-    var div2 = d3.select(container.getElement()[0]).append("div");
-    var brush =[];
-    var update = [];
+    var config = state.config || defaultConfig;
+    //TODO FORM state();
+    //change title form; 
+    cfg.append("input")
+       .attr("type","button")
+       .attr("value","submit")
+       .on("click",function(){
+         cfg.style("display","none"); //jQuery .hide()
+         content.style("display","block"); //jQuery .show()
+         container.extendState({
+           "configToggle":false,
+           "config":config //TODO get config.state();
+         });
+       });
+
+    //container.extendState({"config":config})
+
+    /* render content */
+    var brush = []; // instant states not store in container
+    var update = state.regions || [];
     layout.eventHub.on("brush", function(d) {
         brush = d;
         if(!container.isHidden){
@@ -2481,7 +2512,8 @@ var simple = function(layout, container, state) {
 
     });
     layout.eventHub.on("update", function(d) {
-        update = d;
+       update = d;
+       container.extendState({"regions":update});
        if(!container.isHidden){
          div1.html("CURRENT   " + regionsText(d));
          div2.html("");
--- scope/web/src/renderer/simple.js
+++ scope/web/src/renderer/simple.js
@@ -1,11 +1,42 @@
 import regionText from "../tools/regionText"
 import regionsText from "../tools/regionsText"
+var defaultConfig = {
+  "color" : "#EED"
+}
 export default function(layout, container, state) {
+    //local event driven . cfg, content ...
+    // not d3.call but registerComponent render.
+    // similar with d3.call.
+    // layout is the interface
+    // container is the element.
+    //var dispatch = d3.dispatch("local")
+    var cfg = d3.select(container.getElement()[0]).append("div").classed("cfg",true);
+    var content = d3.select(container.getElement()[0]).append("div").classed("content",true);
+    var div1 = content.append("div");
+    var div2 = content.append("div");
+    //state.config parameters.
+    /* render config panel and configs */
 
-    var div1 = d3.select(container.getElement()[0]).append("div");
-    var div2 = d3.select(container.getElement()[0]).append("div");
-    var brush =[]
-    var update = []
+    var config = state.config || defaultConfig
+    //TODO FORM state();
+    //change title form; 
+    cfg.append("input")
+       .attr("type","button")
+       .attr("value","submit")
+       .on("click",function(){
+         cfg.style("display","none") //jQuery .hide()
+         content.style("display","block") //jQuery .show()
+         container.extendState({
+           "configToggle":false,
+           "config":config //TODO get config.state();
+         })
+       })
+
+    //container.extendState({"config":config})
+
+    /* render content */
+    var brush = [] // instant states not store in container
+    var update = state.regions || []
     layout.eventHub.on("brush", function(d) {
         brush = d
         if(!container.isHidden){
@@ -14,7 +45,8 @@ export default function(layout, container, state) {
 
     })
     layout.eventHub.on("update", function(d) {
-        update = d;
+       update = d;
+       container.extendState({"regions":update});
        if(!container.isHidden){
          div1.html("CURRENT   " + regionsText(d))
          div2.html("")
